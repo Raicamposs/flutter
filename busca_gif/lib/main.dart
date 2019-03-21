edf450a0e9c427f3c0450ab4af071af2413fb32f
Raiane Campos <raianejanu@hotmail.com>
2019-03-20 23:36:13 -0300

    Buscador de gifs

---
+++ busca_gif/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:busca_gif/ui/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+    theme: ThemeData(hintColor: Colors.white),
+  ));
+}
